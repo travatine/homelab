dd32b77d43b08bb4366cff629771bba680bfbe86
Travis <travatine@hotmail.com>
2024-03-20 11:14:15 +0800

    Update install.docker.on.debian12.sh

--- scripts/install.docker.on.debian12.sh
+++ scripts/install.docker.on.debian12.sh
@@ -11,9 +11,11 @@ done
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+if [ ! -f /etc/apt/keyrings/docker.asc ]; then
+  sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+fi
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
@@ -24,7 +26,12 @@ echo \
 sudo apt-get update
 
 # Install 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y\
+ docker-ce \
+ docker-ce-cli \
+ containerd.io \
+ docker-buildx-plugin \
+ docker-compose-plugin
 
 # Test
 sudo docker run hello-world
