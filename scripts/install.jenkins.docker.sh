dbaf0fc521796da2bfd7323179fc03d2d7c274c9
Travis <travatine@hotmail.com>
2024-03-20 11:47:37 +0800

    Update install.jenkins.docker.sh

--- scripts/install.jenkins.docker.sh
+++ scripts/install.jenkins.docker.sh
@@ -18,8 +18,8 @@ docker run \
   --network jenkins \
   --network-alias docker \
   --env DOCKER_TLS_CERTDIR=/certs \
-  --volume $JENKINS_CONFIG_DIR/jenkins-docker-certs:/certs/client \
-  --volume $JENKINS_CONFIG_DIR/jenkins-data:/var/jenkins_home \
+  --volume ./jenkins-docker-certs:/certs/client \
+  --volume ./jenkins-data:/var/jenkins_home \
   --publish 2376:2376 \
   docker:dind \
   --storage-driver overlay2
@@ -54,6 +54,6 @@ docker run \
   --env DOCKER_TLS_VERIFY=1 \
   --publish 8080:8080 \
   --publish 50000:50000 \
-  --volume  $JENKINS_CONFIG_DIR/jenkins-data:/var/jenkins_home \
-  --volume  $JENKINS_CONFIG_DIR/jenkins-docker-certs:/certs/client:ro \
+  --volume  ./jenkins-data:/var/jenkins_home \
+  --volume  ./jenkins-docker-certs:/certs/client:ro \
   myjenkins:lts
